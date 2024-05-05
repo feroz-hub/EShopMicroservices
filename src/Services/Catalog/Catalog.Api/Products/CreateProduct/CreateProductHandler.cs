f71a1357c0bed6bd1247b56836e2de9cd11ebaef
feroz-hub <bashaferoz66@gmail.com>
2024-05-05 20:56:37 +0530

    Catalog Api Post request

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+namespace Catalog.Api.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler(IDocumentSession documentSession):ICommandHandler<CreateProductCommand,CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        documentSession.Store(product);
+        await documentSession.SaveChangesAsync(cancellationToken); 
+        return new CreateProductResult(product.Id);
+    }
+}
