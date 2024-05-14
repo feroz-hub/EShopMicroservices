4c1d2cec77a35c6fda60bbd9b032389484e60428
Kali <bashaferoz027@gmail.com>
2024-05-14 10:05:31 +0530

    Comment

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,14 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 // Add Services to the Container.
-
-
-
 builder.Services
        .AddApplicationServices()
        .AddInfrastructureServices(builder.Configuration)
        .AddApiServices();
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
