990ff006fabe74083551d0f42bf9ace55ab52840
Kali <bashaferoz027@gmail.com>
2024-05-14 08:29:31 +0530

    Ordering.Infrastructure.csproj

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):DbContext(options)
+{
+    public DbSet<Customer> Customers=>Set<Customer>();
+    public DbSet<Order> Orders=>Set<Order>();
+    public DbSet<Product> Products=>Set<Product>();
+    public DbSet<OrderItem> OrderItems=>Set<OrderItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    { 
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
