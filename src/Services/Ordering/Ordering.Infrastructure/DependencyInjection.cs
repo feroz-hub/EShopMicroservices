014a040b5fdc892182118f5c8bf68ebbb3e6cf54
Kali <bashaferoz027@gmail.com>
2024-05-14 10:04:50 +0530

    ApplicationDbContext.cs

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,11 @@
-namespace Ordering.Infrastructure;
-
+namespace Ordering.Infrastructure; 
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration) 
     {
         var connectionString = configuration.GetConnectionString("Database");
-        // Add Service to the Container
-        // services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-        // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+       // Add Service to the Container
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
         return services;
     }
 }
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -1,4 +1,11 @@
 // Global using directives
 
+global using System.Reflection;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using Ordering.Infrastructure.Data;
