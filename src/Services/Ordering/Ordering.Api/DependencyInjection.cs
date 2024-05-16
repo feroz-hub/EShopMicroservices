1717387862874326045137f85215c328f85e44c8
Kali <bashaferoz027@gmail.com>
2024-05-16 15:16:27 +0530

    HealthCheck for Ordering.Api.csproj

--- src/Services/Ordering/Ordering.Api/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,15 +1,24 @@
 namespace Ordering.Api;
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services,IConfiguration configuration)
     {
         services.AddCarter();
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks().AddSqlServer(configuration.GetConnectionString("Database")!);
+
         return services;
     }
     
     public static WebApplication UseApiServices(this WebApplication app)
     {
         app.MapCarter();
+        app.UseExceptionHandler(options=> { });
+        app.UseHealthChecks("/health",new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
         return app;
     }
 }
--- src/Services/Ordering/Ordering.Api/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Api/GlobalUsings.cs
@@ -1,9 +1,12 @@
 // Global using directives
 
+global using BuildingBlocks.Exceptions.Handler;
 global using BuildingBlocks.Pagination;
 global using Carter;
+global using HealthChecks.UI.Client;
 global using Mapster;
 global using MediatR;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 global using Ordering.Api;
 global using Ordering.Application;
 global using Ordering.Application.Dtos;
